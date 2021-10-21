6626841fb17758298a5c98213ea15426f6f3d800
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-10-21 11:25:29 +0200

    created getPredictions()

--- app/app.js
+++ app/app.js
@@ -2,12 +2,18 @@ let form = document.querySelector("form")
 let query = document.querySelector("input[type=text]")
 let submit = document.querySelector("input[type=submit]")
 
+let predictions = []
+
 window.onload = () => {
     validQuery()
 }
 
 query.addEventListener('input', async () => {
     validQuery()
+    if(query.value){
+        predictions = await getPredictions()
+        console.log(predictions)
+    }
 })
 
 form.addEventListener('submit', async (e) => {
@@ -21,4 +27,15 @@ form.addEventListener('submit', async (e) => {
 
 validQuery = () => {
     submit.disabled = (!query.value) ? true : false
+}
+
+getPredictions = async () => {
+    let response = await fetch(`https://restcountries.com/v3.1/name/${query.value}`)
+    let result = await response.json()
+    let names = []
+    let amount = result.length > 10 ? 10 : result.length
+    for (let i = 0; i < amount; i++) {
+        names.push(result[i].name.common)
+    }
+    return names
 }
