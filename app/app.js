378fdd7845523fa969ea8dbfe050577e7a11f711
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-10-21 11:24:35 +0200

    created function validQuery()

--- app/app.js
+++ app/app.js
@@ -2,10 +2,23 @@ let form = document.querySelector("form")
 let query = document.querySelector("input[type=text]")
 let submit = document.querySelector("input[type=submit]")
 
+window.onload = () => {
+    validQuery()
+}
+
+query.addEventListener('input', async () => {
+    validQuery()
+})
+
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
+    console.log(query.value)
     //https://restcountries.com/v3.1/name/(name)
     let response = await fetch(`https://restcountries.com/v3.1/name/${query.value}?fullText=true`)
     let result = await response.json()
     console.log(result)
 })
+
+validQuery = () => {
+    submit.disabled = (!query.value) ? true : false
+}
