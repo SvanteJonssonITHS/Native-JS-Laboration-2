a9d9b88ea0124b4f9fc96e82f683013293a0a0e4
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-11-01 21:00:24 +0100

    renamed and added global variables

--- app/app.js
+++ app/app.js
@@ -1,9 +1,7 @@
-let form = document.querySelector("form")
-let query = document.querySelector("input[type=text]")
-let submit = document.querySelector("input[type=submit]")
-
+let _form = document.querySelector("form")
+let _query = document.querySelector("input[type=text]")
 let _allCountries = []
-let predictions = []
+let _predictionList = document.querySelector('#predictions-wrapper')
 
 window.onload = async () => {
     validQuery()
@@ -11,25 +9,25 @@ window.onload = async () => {
     console.log(_allCountries)
 }
 
-query.addEventListener('input', async () => {
+_query.addEventListener('input', async () => {
     validQuery()
-    if(query.value){
+    if(_query.value){
         predictions = await getPredictions()
         console.log(predictions)
     }
 })
 
-form.addEventListener('submit', async (e) => {
+_form.addEventListener('submit', async (e) => {
     e.preventDefault();
     console.log(query.value)
     //https://restcountries.com/v3.1/name/(name)
-    let response = await fetch(`https://restcountries.com/v3.1/name/${query.value}?fullText=true`)
+    let response = await fetch(`https://restcountries.com/v3.1/name/${_query.value}?fullText=true`)
     let result = await response.json()
     console.log(result)
 })
 
 validQuery = () => {
-    submit.disabled = (!query.value) ? true : false
+    submit.disabled = (!_query.value) ? true : false
 }
 
 getAllCountryNames = async () => {
