013fdad9bcb76cfa97e13432c7db30d33330756e
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-11-01 22:37:22 +0100

    multiple spellings of each country

--- app/app.js
+++ app/app.js
@@ -5,12 +5,26 @@ let _predictionList = document.querySelector('#predictions-wrapper')
 let _activePrediction = -1
 
 window.onload = async () => {
-    _allCountries = await getAllCountryNames()
+    _allCountries = prepareNameArray(await getAllCountryNames())
 }
 
 getAllCountryNames = async () => {
-    let response = await fetch('https://restcountries.com/v2/all?fields=name')
+    let response = await fetch('https://restcountries.com/v2/all?fields=name,nativeName,altSpellings')
     return await response.json()
+}
+
+prepareNameArray = (countries) => {
+    let arr = JSON.parse(localStorage.getItem('allcoutries')) || []
+    if(arr.length == 0) {
+        for (let i = 0; i < countries.length; i++) {
+            const country = countries[i];
+            if(country.name) arr.push({common: country.name, alt: country.name})
+            if(country.nativeName && country.name != country.nativeName) arr.push({common: country.name, alt: country.nativeName})
+            if(country.altSpellings.length && country.name != country.altSpellings[0] && country.nativeName != country.altSpellings[0]) arr.push({common: country.name, alt: country.altSpellings[0]})
+        }
+        localStorage.setItem('allcoutries', JSON.stringify(arr))
+    }
+    return arr
 }
 
 openPredictionList = (element) => {
@@ -38,7 +52,6 @@ createPrediction = (prediction, query) => {
 
 changeActivePrediction = (direction) => {
     let predicitons = document.querySelectorAll('.prediction')
-
     for(let i = 0; i < predicitons.length; i++) {
         let prediciton = predicitons[i]
         if(prediciton.classList.contains('active-prediction')) {
@@ -73,9 +86,10 @@ _query.addEventListener('input', () => {
     if(!query) return false
 
     for(let i = 0; i < _allCountries.length; i++) {
-        let prediciton = _allCountries[i]
-        if(prediciton.name.substr(0, query.length).toLowerCase() == query.toLowerCase()) {
-            _predictionList.appendChild(createPrediction(prediciton.name, query))
+        let prediciton = _allCountries[i].alt
+        if(prediciton.substr(0, query.length).toLowerCase() == query.toLowerCase()) {
+            _predictionList.appendChild(createPrediction(prediciton, query))
+            if(_predictionList.children.length >= 10) break;
         }
     }
 
