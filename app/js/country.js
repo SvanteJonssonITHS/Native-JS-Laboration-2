63011d2087f1ffea0a4e224791926fc272ac01f5
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-11-03 17:27:34 +0100

    created function `getCountry()`

--- app/js/country.js
+++ app/js/country.js
@@ -1,6 +1,14 @@
 const _query = sessionStorage.getItem('country')
 
-window.onload = () => {
+let _country
+
+window.onload = async () => {
     if(!_query) window.location = './index.html'
+    _country = await getCountry(_query)
 }
 
+getCountry = async (name) => {
+    const response = await fetch(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+    const country = await response.json()
+    return country[0]
+}
