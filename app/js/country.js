7acd0ebc5e3ca3bd6b640976fdf5c82c27c9ee6e
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-11-04 16:55:55 +0100

    created function `createCountryOverview()` and currently create two sections of the country overview sidebar

--- app/js/country.js
+++ app/js/country.js
@@ -5,12 +5,32 @@ let _country
 window.onload = async () => {
 	if (!_query) window.location = './index.html'
 	_country = await getCountry(_query)
-	console.log(_country.latlng[0], _country.latlng[1])
-	createMap(_country.cca3, _country.latlng[0], _country.latlng[1])
+	if (_country) createCountryOverview(_country)
 }
 
 getCountry = async (name) => {
 	const response = await fetch(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
 	const country = await response.json()
 	return country[0]
+}
+
+createCountryOverview = (country) => {
+	// Country identity section
+	const identity = document.querySelector('.country-identity')
+	const commonName = document.createElement('h2')
+	commonName.textContent = country.name.common
+	const officialName = document.createElement('h3')
+	officialName.textContent = country.name.official
+	const symbols = document.createElement('div')
+	symbols.setAttribute('class', 'national-symbols-wrapper')
+	const flag = document.createElement('img')
+	flag.setAttribute('src', country.flags.svg)
+	flag.setAttribute('alt', `${country.name.common} flag`)
+	const coa = document.createElement('img')
+	coa.setAttribute('src', country.coatOfArms.svg)
+	coa.setAttribute('alt', `${country.name.common} coat of arms`)
+	symbols.append(flag, coa)
+	identity.append(commonName, officialName, symbols)
+	// Region section
+	createMap(country.cca3, country.latlng[0], country.latlng[1])
 }
