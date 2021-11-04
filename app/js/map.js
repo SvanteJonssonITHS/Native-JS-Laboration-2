e2104c177c62a3e9484cc406665d8b9aa3524e65
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-11-04 15:43:18 +0100

    created function that creates a map

---
+++ app/js/map.js
@@ -0,0 +1,48 @@
+createMap = (cioc) => {
+	const mapStyle = new ol.style.Style({
+		fill: new ol.style.Fill({
+			color: '#567d46'
+		}),
+		stroke: new ol.style.Stroke({
+			color: '#2b3f23',
+			width: 1
+		})
+	})
+
+	const vectorLayer = new ol.layer.Vector({
+		source: new ol.source.Vector({
+			url: 'https://openlayers.org/en/latest/examples/data/geojson/countries.geojson',
+			format: new ol.format.GeoJSON()
+		}),
+		style: mapStyle
+	})
+
+	const map = new ol.Map({
+		layers: [vectorLayer],
+		target: 'map',
+		view: new ol.View({
+			center: [0, 0],
+			zoom: 2
+		})
+	})
+
+	const highlightStyle = new ol.style.Style({
+		stroke: new ol.style.Stroke({
+			color: '#f00',
+			width: 1
+		}),
+		fill: new ol.style.Fill({
+			color: 'rgba(255,0,0,0.1)'
+		})
+	})
+
+	const featureOverlay = new ol.layer.Vector({
+		source: new ol.source.Vector(),
+		map: map,
+		style: highlightStyle
+	})
+
+	map.on('postrender', () => {
+	})
+}
+
