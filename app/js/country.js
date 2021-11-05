0f07ec87194e701b370fef22a5898d1ca00fdf3b
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-11-05 20:53:48 +0100

    created function `generateSummary()`

--- app/js/country.js
+++ app/js/country.js
@@ -46,7 +46,86 @@ createMainContent = async (country, borderingCountries) => {
 	const summaryTitle = document.createElement('h2')
 	summaryTitle.textContent = 'Summary'
 	const summaryText = document.createElement('p')
+	summaryText.textContent = Object.values(await generateSummary(country, borderingCountries)).join(' ')
 	summary.append(summaryTitle, summaryText)
+}
+
+generateSummary = async (country, borderingCountries) => {
+	let summary = {}
+
+	// Location sentence
+	if (country.name.common && country.continents[0].length > 0) {
+		summary.location = `${country.name.common} is a country located in ${country.continents[0]}`
+		if (country.borders && country.borders.length > 0) {
+			summary.location += ` and shares borders with `
+			const borders = borderingCountries.map(({ name }) => name.common)
+			if (borders.length > 3) {
+				summary.location += `${borders[0]}, ${borders[1]} and ${borders[2]} among others.`
+			} else if (borders.length == 3) {
+				summary.location += `${borders[0]}, ${borders[1]} and ${borders[2]}.`
+			} else if (borders.length == 2) {
+				summary.location += `${borders[0]} and ${borders[1]}.`
+			} else {
+				summary.location += `${borders[0]}.`
+			}
+		} else {
+			summary.location += '.'
+		}
+	}
+
+	// Capital sentence
+	if (country.capital && country.capital[0].length > 0) {
+		summary.capital = `Its capital, ${country.capital[0]}`
+		if (country.capitalInfo.latlng && country.capitalInfo.latlng.length > 0) {
+			summary.capital += `, is located at the coordinates ${country.capitalInfo.latlng[0]}&#176;N,  ${country.capitalInfo.latlng[1]}&#176;E.`
+		} else {
+			summary.capital += '.'
+		}
+	}
+
+	// Size sentence
+	if (country.name.common && country.population) {
+		summary.size = `The total population of ${country.name.common} is currently ${Intl.NumberFormat().format(country.population)}`
+		if (country.area) {
+			summary.size += `, and it has a total land area of ${Intl.NumberFormat().format(country.area)} km&#178;.`
+		} else {
+			summary.size += '.'
+		}
+	}
+
+	// Economy sentence
+	if (country.gini) {
+		const giniWorldAverage = await getWorldGiniAverage()
+		let aboveWorldAverage
+		switch (country.gini > giniWorldAverage) {
+			case true:
+				aboveWorldAverage = 'above'
+				break
+			case false:
+				aboveWorldAverage = 'below'
+				break
+		}
+		summary.economy = `The country's distrubution of income among its citizens(Gini index, lower is better) is, ${Object.values(country.gini)[0]}, and is ${aboveWorldAverage} the world average of ${giniWorldAverage}.`
+	}
+
+	// Traffic sentence
+	if (country.car.side) {
+		summary.traffic = `They drive on the ${country.car.side} side of the road`
+		if (country.car.signs && country.car.signs.length > 0) {
+			summary.traffic += ` with license plates starting with `
+			for (let i = 0; i < country.car.signs.length; i++) {
+				summary.traffic += `${country.car.signs[i]}, `
+			}
+			summary.traffic += `${summary.traffic.slice(0, -2)}.`
+		} else {
+			summary.traffic += '.'
+		}
+	}
+
+	// Calendar sentence
+	if (country.startOfWeek) summary.calendar = `Its calendar week starts on ${country.startOfWeek}s.`
+
+	return summary
 }
 
 getBorderingCountries = async (codes) => {
